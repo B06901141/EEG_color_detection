2cf607d12182867587cd8f8fadd85cdc8e79eff1
B06901141 <b06901141@ntu.edu.tw>
2019-11-30 12:16:08 +0800

    done reading data

--- src/readData.py
+++ src/readData.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 from itertools import permutations
 
+import tqdm
 import scipy.io as sio
 
 """
@@ -20,26 +21,69 @@ array(['EEG', 'alpha', 'beta', 'delta', 'theta']
 40-50 color3
 """
 
-def readDataSet(dataPath):
-    color = ["".join(i) for i in permutations("rgb")]
+time2index = lambda s,f=200: int(s*f)
+def readDataSet(dataPath, cmap ="rgb"):
+    color = ["".join(i) for i in permutations(cmap)]
     data = {i:[] for i in color}
 
     dataFolder = [os.path.join(dataPath, i) for i in os.listdir(dataPath) if i != ".git"]
     dataFolder = [i for i in dataFolder if os.path.isdir(i)]
     matFiles = [[j for j in os.listdir(i) if j.split(".")[-1] == "mat"] for i in dataFolder]
 
-    for folder, matList in zip(dataFolder, matFiles):
+    print("Reading data...")
+    for folder, matList in zip(tqdm.tqdm(dataFolder, ncols = 70), matFiles):
         for mat in matList:
             fileName = os.path.join(folder, mat)
-            if mat.split("_")[0] not in color:
-                print("Warning: File %s doesn't start with either \"%s\", skipping..."%(fileName,"\" or \"".join(color)))
+            colorOrder = mat.split(".")[0].split("_")[-1]
+            if colorOrder not in color:
+                print("Warning: File %s doesn't end with either \"%s\", skipping..."%(fileName,"\" or \"".join(color)))
                 continue
-            data[mat.split("_")[0]].append(sio.loadmat(fileName)["data"])
+            data[colorOrder].append(sio.loadmat(fileName)["data"])
     return data
 def normalize(data):
-    #TODO
-    pass
+    print("Normalizing data...")
+    for waveform in tqdm.tqdm(data.values(), ncols = 70):
+        for i in range(len(waveform)):
+            base = waveform[i][:time2index(9.5)]
+            mean = base.mean(axis = 0)
+            std = base.std(axis = 0)
+            waveform[i] = (waveform[i] - mean) / std
+    return data
+def splitColor(data, cmap = "rgb"):
+    d = {i:[] for i in cmap}
+    d["dummy"] = []
+    time_start = [10.5,25.5,40.5,0.5]
+    time_end = [19.5,34.5,49.5,9.5]
+    index_start = [time2index(i) for i in time_start]
+    index_end = [time2index(i) for i in time_end]
+    for color, waveform in data.items():
+        for i in range(3):
+            d[cmap[i]].extend([wave[index_start[i]:index_end[i]] for wave in waveform])
+        i = -1
+        d["dummy"].extend([wave[index_start[i]:index_end[i]] for wave in waveform])
+    for i in d:
+        d[i] = np.array(d[i], dtype=np.float32)
+    return d
+def splitData(data, testNum = 1):
+    test_data = {i:j[-testNum:, ...] for i,j in data.items()}
+    train_data = {i:j[:-testNum, ...] for i,j in data.items()}
+    return train_data, test_data
+def cropData(data, cropSize = 512, step = 256):
+    for color in data:
+        totalTime = data[color].shape[1]
+        cropped = []
+        for time in range(0,totalTime-cropSize,step):
+            cropped.extend(data[color][:,time:time+cropSize,:])
+        data[color] = np.array(cropped, dtype=np.float32)
+    return data
 
 if __name__ == '__main__':
     data = readDataSet("../dataset")
-    print(data)
+    data = normalize(data)
+    data = splitColor(data)
+    data = cropData(data)
+    data_train, data_test = splitData(data)
+    print(data_train.keys())
+    for wave in data_train.values():
+        print(wave.shape) #(datanum, time, channel)
+
